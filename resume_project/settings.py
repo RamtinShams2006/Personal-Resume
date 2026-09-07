336c3811bcc5c3d6b36cc974d15294b7a982dc78
RamtinShams <ramtin.shams.oskouie2006@gmail.com>
2026-09-07 20:41:26 +0330

    prepare project for deployment

--- resume_project/settings.py
+++ resume_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w$blp!py^uu0^+&1@xt^-vqf(-r-!+1mqr@ty6-6xey*!w#3_&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
